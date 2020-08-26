a5eec18eb7add63353505ef5ef7b8307e6e82fbc
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-08-27 00:12:26 +0300

    added variable edits

--- js/task8.js
+++ js/task8.js
@@ -14,9 +14,9 @@ function createBox(size) {
 
 
 function getRgb() {
-  let r = Math.floor(Math.random()*256);
-  let g = Math.floor(Math.random()*256);
-  let b = Math.floor(Math.random()*256);
+  const r = Math.floor(Math.random()*256);
+  const g = Math.floor(Math.random()*256);
+  const b = Math.floor(Math.random()*256);
   return `rgb(${r}, ${g}, ${b})`;
 };
 
